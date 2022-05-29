620c190f001547c25856538b2da6169a1e147cfd
Mattori Birnbaum <mattori.birnbaum@gmail.com>
2022-05-29 20:08:32 +0900

    starting to implement

---
+++ SamplePlugin/ActionWatcher.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dalamud.Logging;
+
+namespace SamplePlugin {
+    public class CastState {
+        public readonly uint actionId;
+        public readonly float castTime;
+
+        public CastState() {
+            actionId = 0;
+            castTime = 0.0f;
+        }
+
+        public CastState(uint actionId, float castTime) {
+            this.actionId = actionId;
+            this.castTime = castTime;
+        }
+
+        // individual casts don't have any sort of id, so check if same action
+        // and if casting time has increased
+        public bool IsSameCast(CastState other) {
+            return actionId == other.actionId && castTime < other.castTime;
+        }
+    }
+
+    public class ActionWatcher : IDisposable {
+        private bool _enabled;
+        public CastState? lastCast { get; private set; } = null;
+
+        public ActionWatcher(bool enabled) {
+            _enabled = enabled;
+        }
+
+        public void Enable() {
+            if (_enabled) {
+                return;
+            }
+
+            Dalamud.Framework.Update += OnFrameworkUpdate;
+            _enabled = true;
+        }
+
+        public void Disable() {
+            if (!_enabled) {
+                return;
+            }
+
+            Dalamud.Framework.Update -= OnFrameworkUpdate;
+            _enabled = false;
+        }
+
+        public void Dispose() => Disable();
+
+        private void CheckCast() {
+            var player = Dalamud.ClientState.LocalPlayer;
+
+            if (player == null) {
+                PluginLog.Error("player data could not be loaded");
+                return;
+            }
+
+            var cast = new CastState(player.CastActionId, player.CurrentCastTime);
+
+            if (lastCast!.IsSameCast(cast)) {
+                lastCast = cast;
+            }
+        }
+
+        public void OnFrameworkUpdate(object _) {
+            CheckCast();
+        }
+    }
+}
--- SamplePlugin/Plugin.cs
+++ SamplePlugin/Plugin.cs
@@ -1,13 +1,12 @@
-﻿using Dalamud.Game.Command;
+﻿using Dalamud.Game;
+using Dalamud.Game.Command;
 using Dalamud.IoC;
 using Dalamud.Plugin;
 using System.IO;
 using System.Reflection;
 
-namespace SamplePlugin
-{
-    public sealed class Plugin : IDalamudPlugin
-    {
+namespace SamplePlugin {
+    public sealed partial class Plugin : IDalamudPlugin {
         public string Name => "Sample Plugin";
 
         private const string commandName = "/pmycommand";
@@ -16,11 +15,12 @@ namespace SamplePlugin
         private CommandManager CommandManager { get; init; }
         private Configuration Configuration { get; init; }
         private PluginUI PluginUi { get; init; }
+        
 
         public Plugin(
             [RequiredVersion("1.0")] DalamudPluginInterface pluginInterface,
-            [RequiredVersion("1.0")] CommandManager commandManager)
-        {
+            [RequiredVersion("1.0")] CommandManager commandManager) {
+
             this.PluginInterface = pluginInterface;
             this.CommandManager = commandManager;
 
@@ -32,8 +32,7 @@ namespace SamplePlugin
             var goatImage = this.PluginInterface.UiBuilder.LoadImage(imagePath);
             this.PluginUi = new PluginUI(this.Configuration, goatImage);
 
-            this.CommandManager.AddHandler(commandName, new CommandInfo(OnCommand)
-            {
+            this.CommandManager.AddHandler(commandName, new CommandInfo(OnCommand) {
                 HelpMessage = "A useful message to display in /xlhelp"
             });
 
@@ -41,25 +40,21 @@ namespace SamplePlugin
             this.PluginInterface.UiBuilder.OpenConfigUi += DrawConfigUI;
         }
 
-        public void Dispose()
-        {
+        public void Dispose() {
             this.PluginUi.Dispose();
             this.CommandManager.RemoveHandler(commandName);
         }
 
-        private void OnCommand(string command, string args)
-        {
+        private void OnCommand(string command, string args) {
             // in response to the slash command, just display our main ui
             this.PluginUi.Visible = true;
         }
 
-        private void DrawUI()
-        {
+        private void DrawUI() {
             this.PluginUi.Draw();
         }
 
-        private void DrawConfigUI()
-        {
+        private void DrawConfigUI() {
             this.PluginUi.SettingsVisible = true;
         }
     }
